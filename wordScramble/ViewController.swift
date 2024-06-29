56829fd5ed825cc3cc63e865e6bc3be041eababc
Ferin.Chow <nrrri.cho@gmail.com>
2024-06-29 19:37:14 -0400

    Initial Commit

---
+++ wordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordScramble
+//
+//  Created by Narissorn Chowarun on 2024-06-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
